2cd179809a7b57d3f7d37631d1c00739869e98af
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-10 16:36:41 +0100

    6.3. Added bcrypt authentication lifecycle hooks to the user model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
@@ -8,5 +9,28 @@ const userSchema = new mongoose.Schema({
 }, {
   id: false
 });
+
+userSchema.virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation){
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function checkPasswordsMatch(next){
+  if(this.isModified('password') && this._passwordConfirmation !== this.password){
+    this.invalidate('passwordConfirmation', 'does not match');
+  }
+  next();
+});
+
+userSchema.pre('save', function hashPassword(next){
+  if(this.isModified('password')){
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+userSchema.methods.validatePassword = function validatePassword(password){
+  return bcrypt.compareSync(this.password, password);
+};
 
 module.exports = mongoose.model('User', userSchema);
