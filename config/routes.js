d77d71781613ff9cf0e08f0d893a39fa271f623e
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-10 16:19:28 +0100

    6.2. Made user/auth register route

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 
 const treatments = require('../controllers/treatments');
+const auth = require('../controllers/auth');
 
 routes.route('/treatments')
   .get(treatments.index)
@@ -10,5 +11,8 @@ routes.route('/treatments/:id')
   .get(treatments.show)
   .put(treatments.update)
   .delete(treatments.delete);
+
+routes.route('/register')
+  .post(auth.register);
 
 module.exports = routes;
