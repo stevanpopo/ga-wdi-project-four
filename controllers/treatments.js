daef9322b7bc613f72df7a7e236cafc2b8f75b03
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-10 15:36:45 +0100

    5.7. Made treatment update route

--- controllers/treatments.js
+++ controllers/treatments.js
@@ -14,12 +14,21 @@ function showRoute(req, res, next){
 
 function createRoute(req, res, next){
   Treatment.create(req.body)
-    .then(treatment => res.json(treatment))
+    .then(treatment => res.status(201).json(treatment))
+    .catch(next);
+}
+
+function updateRoute(req, res, next){
+  Treatment.findById(req.params.id)
+    .then(treatment => treatment.set(req.body))
+    .then(treatment => treatment.save())
+    .then(treatment => treatment.json())
     .catch(next);
 }
 
 module.exports = {
   index: indexRoute,
   show: showRoute,
-  create: createRoute
+  create: createRoute,
+  update: updateRoute
 };
