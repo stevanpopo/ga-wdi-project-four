4b019e96324a398e2e89f6830b98681b914e7f23
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-11 12:14:39 +0100

    7.6. Created front-end TreatmentsNew component

---
+++ src/components/treatments/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Form = () => {
+  return(
+    <section>
+      <div>
+        <form>
+          <div className="field">
+            <label className="label">Title</label>
+            <input className="input" />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Form;
---
+++ src/components/treatments/New.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TreatmentsNew extends React.Component{
+  constructor(){
+    super();
+    this.state = {};
+  }
+
+
+}
+
+export default TreatmentsNew;
