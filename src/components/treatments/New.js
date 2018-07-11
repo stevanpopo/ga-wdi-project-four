95ba0b2e634d97614e048ae1ebd2be6df3b54367
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-11 12:30:41 +0100

    7.7. Added form to the render TreatmentsNew

--- src/components/treatments/New.js
+++ src/components/treatments/New.js
@@ -21,7 +21,19 @@ class TreatmentsNew extends React.Component{
           <form>
             <div className="field">
               <label className="label">Title</label>
-              <input className="input" />
+              <input className="input" name="title" placeholder="Title" onChange={handleChange} value={data.title || ''} />
+            </div>
+            <div className="field">
+              <label className="label">Date & Time</label>
+              <input className="input" name="dateTime" placeholder="Date & Time" onChange={handleChange} value={data.dateTime || ''} />
+            </div>
+            <div className="field">
+              <label className="label">Image</label>
+              <input className="input" name="image" placeholder="Image" onChange={handleChange} value={data.image || ''} />
+            </div>
+            <div className="field">
+              <label className="label">Notes</label>
+              <input className="input" name="notes" placeholder="Notes" onChange={handleChange} value={data.notes || ''} />
             </div>
           </form>
         </div>
