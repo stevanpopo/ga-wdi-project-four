882b57f3e137bbc77a920663f770393b8e4284dc
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-12 18:29:44 +0100

    20.1. Created the user show component

---
+++ src/components/user/Show.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class UserShow extends React.Component{
+  state = {}
+
+  render(){
+    return(
+      <section>
+        <h1>User Show Page</h1>
+      </section>
+    );
+  }
+}
+
+export default UserShow;
