3965a29cff3e1c05b96034aed7de072bef9b246c
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-13 10:25:28 +0100

    21.3. Added show route to record controller

--- controllers/records.js
+++ controllers/records.js
@@ -6,6 +6,12 @@ function createRoute(req, res, next){
     .catch(next);
 }
 
+function showRoute(req, res, next){
+  Record.findById(req.params.id)
+    .then(record => res.json(record))
+    .catch(next);
+}
+
 function updateRoute(req, res, next){
   Record.findById(req.params.id)
     .then(record => record.set(req.body))
@@ -16,5 +22,6 @@ function updateRoute(req, res, next){
 
 module.exports = {
   create: createRoute,
-  update: updateRoute
+  update: updateRoute,
+  show: showRoute
 };
