13869c6b4c112afa993c27f839417a94a5c8f755
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-13 09:25:39 +0100

    Filled out the user edit component

--- src/components/users/Edit.js
+++ src/components/users/Edit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class UsersEdit extends React.Component{
 
@@ -7,14 +8,34 @@ class UsersEdit extends React.Component{
     this.state = {};
   }
 
+  componentDidMount(){
+    axios.get(`/api/users/${this.props.match.params.id}`)
+      .then(res => this.setState(res.data))
+      .catch(err => console.log('err', err));
+  }
+
   render(){
     console.log('this.state', this.state);
     return(
       <form>
         <div className="field">
           <label className="label">Username</label>
-          <input className="input" name="username" placeholder="Username" />
+          <input className="input" name="username" placeholder="Username" value={this.state.username || ''} />
         </div>
+        <div className="field">
+          <label className="label">Email</label>
+          <input className="input" name="email" placeholder="Email" value={this.state.email || ''} />
+        </div>
+        <div className="field">
+          <label className="label">Password</label>
+          <input className="input" type="password" name="password" placeholder="Password" value={this.state.password || ''} />
+        </div>
+        <div className="field">
+          <label className="label">Password Confirmation</label>
+          <input className="input" type="password" name="passwordConfrimation" placeholder="Password Confirmation" value={this.state.passwordConfrimation || ''} />
+        </div>
+
+        <button className="button">Submit</button>
       </form>
     );
   }
