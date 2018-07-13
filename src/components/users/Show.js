ed4409c496c2d9ef2a5c0d626b4d098f869fe1f7
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-13 10:08:13 +0100

    21. Added form for medical record input to user show page

--- src/components/users/Show.js
+++ src/components/users/Show.js
@@ -22,6 +22,25 @@ class UsersShow extends React.Component{
         <h1>{this.state.user.username}</h1>
         <h1>{this.state.user.email}</h1>
         <Link to={`/users/${this.state.user._id}/edit`} className="button">Edit</Link>
+        
+        <section>
+          <h3 className="title is-3">Add Medical Record</h3>
+          <form>
+            <input className="input" type="hidden" name="owner" placeholder="Owner" value={this.state.user._id} />
+            <div className="field">
+              <label className="label">Weight</label>
+              <input className="input" name="weight" placeholder="Weight" />
+            </div>
+            <div className="field">
+              <label className="label">Glucose</label>
+              <input className="input" name="glucose" placeholder="Glucose" />
+            </div>
+            <div className="field">
+              <label className="label">Blood</label>
+              <input className="input" name="blood" placeholder="Blood" />
+            </div>
+          </form>
+        </section>
       </section>
     );
   }
