46bee8f8c859a4cac2892ed36343ada232b9333f
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-15 20:51:17 +0100

    Added twlio key in the environment file

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,7 @@
 const port = 4000;
 const dbURI = 'mongodb://localhost:27017/yourcare';
 const secret = 'nsh72=<!shiw';
+const twilioKey = process.env.TWILIO_AUTH_KEY;
+const twilioSid = process.env.TWILIO_SID;
 
-module.exports = { port, dbURI, secret };
+module.exports = { port, dbURI, secret, twilioKey, twilioSid };
