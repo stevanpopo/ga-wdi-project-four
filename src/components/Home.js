b6883baa0ea7b59d1888687d6a8c4a30ed5f20a2
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-15 15:39:45 +0100

    Homepage styling changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Home = () => {
   return(
-    <section>
-      <h1>Home page</h1>
+    <section className="container">
+      <div className="columns">
+        <div className="column">
+          <img src="../assets/images/yourcare_demonstration_final.gif" alt="product demo" />
+        </div>
+        <div className="column">
+          <h1>Sign up now!</h1>
+        </div>
+      </div>
     </section>
   );
 };
