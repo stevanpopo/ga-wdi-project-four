d0e74c579c9b17b76edf459247f2a307d314fb44
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-17 13:50:01 +0100

    27.1. Added before to front-end form test

--- test/client/treatments/Index_spec.js
+++ test/client/treatments/Index_spec.js
@@ -7,5 +7,23 @@ import { shallow } from 'enzyme';
 import TreatmentsForm from '../../../src/components/treatments/Form';
 
 describe('TreatmentsForm tests', () => {
-  
+
+  let wrapper;
+
+  beforeEach(done => {
+    const props = {
+      data: {
+        dateTime: new Date('July 14, 2018 06:00:00'),
+        title: 'Morning pills x 3',
+        completed: false,
+        // owner: users[0],
+        image: 'https://cdn.pixabay.com/photo/2016/12/05/19/43/pill-1884775_960_720.jpg',
+        notes: 'To be taken before eating breakfast.'
+      }
+    };
+
+    wrapper = shallow(<TreatmentsForm {...props} />);
+    done();
+  });
+
 })
