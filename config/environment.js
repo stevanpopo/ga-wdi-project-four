57ab82b49961f457622ea9f0cbc2aa24ac2f2698
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-17 20:43:02 +0100

    Changes to environment. Preparing for deployment

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
-const port = 4000;
-const dbURI = 'mongodb://localhost:27017/yourcare';
+const port = process.env.PORT || 4000;
+const dbURI = process.env.MONGODB_URI ||  'mongodb://localhost:27017/yourcare';
 const secret = 'nsh72=<!shiw';
 const twilioKey = process.env.TWILIO_AUTH_KEY;
 const twilioSid = process.env.TWILIO_SID;
