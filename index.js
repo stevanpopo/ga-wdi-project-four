0a183db56f044c67c001028296a0f2b5ff4a2938
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-18 10:11:07 +0100

    trying a fix for favicon icon requests

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ const { port, dbURI } = require('./config/environment');
 app.use(express.static(`${__dirname}/public`));
 mongoose.connect(dbURI);
 
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
-
 app.use(bodyParser.json());
 app.use('/api', routes);
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
-
 module.exports = app;
